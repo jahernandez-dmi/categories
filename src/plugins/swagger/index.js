23295dd1c29ccdafbc76ba1b8848a6ae24be6001
Maximiliano Stahl <maximilianostahl@MacBook-Pro-de-Maximiliano-2.local>
2020-10-19 12:53:04 -0300

    fastify-oas was removed temporally and swagger was disabled

--- src/plugins/swagger/index.js
+++ src/plugins/swagger/index.js
@@ -1,31 +1,31 @@
-const fastifyPlugin = require('fastify-plugin');
-const fastifyOas = require('fastify-oas');
+// const fastifyPlugin = require('fastify-plugin');
+// const fastifyOas = require('fastify-oas');
 
-module.exports = fastifyPlugin((fastify, opts, next) => {
-  const swaggerConfig = {
-    exposeRoute: true,
-    routePrefix: '/docs',
-    swagger: {
-      info: {
-        title: 'Template Microservice',
-        description: 'Template Microservice description',
-        version: '1.0.0'
-      },
-      schemes: ['https'],
-      consumes: ['application/json'],
-      produces: ['application/json'],
-      securityDefinitions: {
-        apiKey: {
-          type: 'apiKey',
-          name: 'apiKey',
-          in: 'header'
-        }
-      },
-      ...opts
-    }
-  };
+// module.exports = fastifyPlugin((fastify, opts, next) => {
+//   const swaggerConfig = {
+//     exposeRoute: true,
+//     routePrefix: '/docs',
+//     swagger: {
+//       info: {
+//         title: 'Template Microservice',
+//         description: 'Template Microservice description',
+//         version: '1.0.0'
+//       },
+//       schemes: ['https'],
+//       consumes: ['application/json'],
+//       produces: ['application/json'],
+//       securityDefinitions: {
+//         apiKey: {
+//           type: 'apiKey',
+//           name: 'apiKey',
+//           in: 'header'
+//         }
+//       },
+//       ...opts
+//     }
+//   };
 
-  fastify.register(fastifyOas, swaggerConfig);
+//   fastify.register(fastifyOas, swaggerConfig);
 
-  next();
-});
+//   next();
+// });
